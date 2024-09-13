63e9c7ef36b848b0f2ec34848b588cceeadaa34d
Arad Afraz <threewestrick@gmail.com>
2024-09-13 17:39:11 +0330

    second

--- backend/local.py
+++ backend/local.py
@@ -1,17 +1,18 @@
 from .settings import *
 
-if os.getenv('DATABASE') == "postgres":
+if os.getenv("DATABASE") == "postgres":
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('POSTGRES_DB'),
-            'USER': os.getenv('POSTGRES_USER'),
-            'PASSWORD': os.getenv('postgres'),
-            'HOST': os.getenv('POSTGRES_HOST'),
-            'PORT': os.getenv('POSTGRES_PORT'),
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB"),
+            "USER": os.getenv("POSTGRES_USER"),
+            "PASSWORD": os.getenv("postgres"),
+            "HOST": os.getenv("POSTGRES_HOST"),
+            "PORT": os.getenv("POSTGRES_PORT"),
         }
     }
 
     if os.getenv("POSTGRES_PATH"):
-        DATABASES['default']['OPTIONS'] = {"options": f'-c search_path={os.getenv("POSTGRES_PATH")}'}
-
+        DATABASES["default"]["OPTIONS"] = {
+            "options": f'-c search_path={os.getenv("POSTGRES_PATH")}'
+        }
